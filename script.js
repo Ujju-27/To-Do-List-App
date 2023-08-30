58b8fd4f1fda91288d7fc1c65a0b14d772534dcc
Ujju-27 <ujjawaltolia853@gmail.com>
2023-08-30 15:21:10 +0530

    Files Upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let inputBx = document.querySelector("#inputBx");
+let list = document.querySelector("#list");
+
+inputBx.addEventListener("keyup", function(event){
+    if(event.key == "Enter")
+    {
+        addItem(this.value)
+        this.value = ""
+    }
+})
+let addItem = (inputBx) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inputBx}<i></i>`;
+        listItem.addEventListener("click", function(){
+            this.classList.toggle('Done');
+        })
+        listItem.querySelector("i").addEventListener("click", function(){
+            listItem.remove();
+        })
+
+        list.appendChild(listItem);
+}
